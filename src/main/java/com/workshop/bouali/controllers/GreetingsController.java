75f2497dd8f79c653e4fb7995da6182f2131cd2c
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-16 12:47:23 +0300

    Add basic security

--- src/main/java/com/workshop/bouali/controllers/GreetingsController.java
+++ src/main/java/com/workshop/bouali/controllers/GreetingsController.java
@@ -1,4 +1,4 @@
-package com.workshop.bouali;
+package  com.workshop.bouali.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
